46852b38f387d0428329824101018733c3dbec5f
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-06-29 19:23:44 +0530

    converted to gitbook
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/codingblocks/codingblocks/view/fragments/BookPageFragment.java
+++ app/src/main/java/com/codingblocks/codingblocks/view/fragments/BookPageFragment.java
@@ -10,12 +10,20 @@ import android.view.ViewGroup;
 
 import com.codingblocks.codingblocks.R;
 
-import us.feras.mdv.MarkdownView;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.tiagohm.markdownview.MarkdownView;
+import br.tiagohm.markdownview.css.ExternalStyleSheet;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BookPageFragment extends Fragment {
+
 
 
     MarkdownView markdownView;
@@ -29,13 +37,78 @@ public class BookPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_book_page, container, false);
-        markdownView = (MarkdownView) root.findViewById(R.id.markdownView);
+        markdownView = (MarkdownView) root.findViewById(R.id.markdown_view);
         Log.d(TAG, "onCreateView: ");
-        markdownView.loadMarkdownFile("file:///android_asset/" +
-                "blocks-of-javascript/01-getting-started/01-1-running-javascript-code.md"
-                , "file:///android_asset/" + "blocks-of-javascript/styles/website.css");
+        markdownView.addStyleSheet(ExternalStyleSheet.fromAsset("blocks-of-javascript/styles/website.css",null));
+        markdownView.loadMarkdownFromAsset("blocks-of-javascript/01-getting-started/01-1-running-javascript-code.md");
+
+//        listAssetFiles("blocks-of-javascript");
+//        for (File file : files){
+//            Log.d(TAG, "onCreateView: " + file.getName());
+//        }
+//        String[] f = new String[0];
+//        ArrayList<String[]> innerFiles = new ArrayList<>();
+//
+//        try {
+//            f = getActivity().getAssets().list("blocks-of-javascript");
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        for (String f1 : f){
+//            Log.d(TAG, "onCreateView: File name : " + f1);
+//            try {
+//                innerFiles.add(getActivity().getAssets().list("blocks-of-javascript/" + f1));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        for (String[] f3 : innerFiles){
+//            for (String f4 : f3){
+//                Log.d(TAG, "onCreateView: " + f4);
+//            }
+//        }
 
         return root;
+    }
+
+    private List<File> getListFiles(File parentDir) {
+        ArrayList<File> inFiles = new ArrayList<File>();
+        File[] files = parentDir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                inFiles.addAll(getListFiles(file));
+            } else {
+                if(file.getName().endsWith(".csv")){
+                    inFiles.add(file);
+                }
+            }
+        }
+        return inFiles;
+    }
+
+    private boolean listAssetFiles(String path) {
+
+        String [] list;
+        try {
+            list = getActivity().getAssets().list(path);
+            if (list.length > 0) {
+                // This is a folder
+                for (String file : list) {
+                    if (!listAssetFiles(path + "/" + file))
+                        return false;
+                }
+            } else {
+                // This is a file
+                // TODO: add file name to an array list
+            }
+        } catch (IOException e) {
+            return false;
+        }
+
+        return true;
     }
 
 }
