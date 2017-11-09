bff66cc3608bde97c1d30c5ac4520dbf841a14b9
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-09 13:55:13 +0530

    network connection check
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/codingblocks/codingblocks/view/activities/BookBaseActivity.java
+++ app/src/main/java/com/codingblocks/codingblocks/view/activities/BookBaseActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -30,6 +32,7 @@ import com.codingblocks.codingblocks.models.BookData;
 import com.codingblocks.codingblocks.models.Chapter;
 import com.codingblocks.codingblocks.models.Contents;
 import com.codingblocks.codingblocks.utils.Constants;
+import com.codingblocks.codingblocks.utils.UtilityMethods;
 import com.codingblocks.codingblocks.view.fragments.BookPageFragment;
 
 import java.util.ArrayList;
@@ -76,57 +79,60 @@ public class BookBaseActivity extends AppCompatActivity
             expandableListView.setBackgroundColor(Color.parseColor("#fafafa"));
         }else{
             expandableListView.setBackgroundColor(Color.parseColor("#2d3143"));
+
         }
-        API.getInstance()
-                .retrofit
-                .create(GitbookAPI.class)
-                .getThisBookChapters(bookAuthor, bookId)
-                .enqueue(new Callback<Contents>() {
-                    @Override
-                    public void onResponse(Call<Contents> call, Response<Contents> response) {
+        if(UtilityMethods.isNetworkConnected(this)) {
+            API.getInstance(BookBaseActivity.this)
+                    .retrofit
+                    .create(GitbookAPI.class)
+                    .getThisBookChapters(bookAuthor, bookId)
+                    .enqueue(new Callback<Contents>() {
+                        @Override
+                        public void onResponse(Call<Contents> call, Response<Contents> response) {
 
-                        Log.d(TAG, "onResponse: " + response);
+                            Log.d(TAG, "onResponse: " + response);
 
-                        Pattern pattern = Pattern.compile("(?<=^| )\\d+(\\.\\d+)?(?=$| )");
-                        String thisGroup = "";
-                        ArrayList<Chapter> thisGroupChild = new ArrayList<Chapter>();
-                        if(response.body() != null) {
-                            bookIntro = response.body().getSections().get(0).getContent();
+                            Pattern pattern = Pattern.compile("(?<=^| )\\d+(\\.\\d+)?(?=$| )");
+                            String thisGroup = "";
+                            ArrayList<Chapter> thisGroupChild = new ArrayList<Chapter>();
+                            if (response.body() != null) {
+                                bookIntro = response.body().getSections().get(0).getContent();
 
-                            if (response.body().getProgress().getChapters() != null) {
-                                for (Chapter chapter : response.body().getProgress().getChapters()) {
-                                    if (pattern.matcher(chapter.getLevel()).matches()) {
-                                        thisGroup = chapter.getTitle();
-                                        groupList.add(chapter);
-                                        thisGroupChild = new ArrayList<Chapter>();
-                                        Log.d(TAG, "onResponse: if" + thisGroup);
+                                if (response.body().getProgress().getChapters() != null) {
+                                    for (Chapter chapter : response.body().getProgress().getChapters()) {
+                                        if (pattern.matcher(chapter.getLevel()).matches()) {
+                                            thisGroup = chapter.getTitle();
+                                            groupList.add(chapter);
+                                            thisGroupChild = new ArrayList<Chapter>();
+                                            Log.d(TAG, "onResponse: if" + thisGroup);
 
-                                        if(chapter.getIndex() != 0){
+                                            if (chapter.getIndex() != 0) {
+                                                thisGroupChild.add(chapter);
+                                            }
+                                            childMap.put(thisGroup, thisGroupChild);
+                                        } else {
                                             thisGroupChild.add(chapter);
+                                            childMap.put(thisGroup, thisGroupChild);
+                                            Log.d(TAG, "onResponse: else" + chapter.getTitle());
                                         }
-                                        childMap.put(thisGroup, thisGroupChild);
-                                    } else {
-                                        thisGroupChild.add(chapter);
-                                        childMap.put(thisGroup, thisGroupChild);
-                                        Log.d(TAG, "onResponse: else" + chapter.getTitle());
                                     }
                                 }
-                            }
-                            fragment = BookPageFragment.getInstance(bookIntro,themeId);
-                            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                            fragmentTransaction.add(R.id.flBookFrame, fragment);
-                            fragmentTransaction.commit();
+                                fragment = BookPageFragment.getInstance(bookIntro, themeId);
+                                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                                fragmentTransaction.add(R.id.flBookFrame, fragment);
+                                fragmentTransaction.commit();
 
-                            exapndableListAdapter.notifyDataSetChanged();
+                                exapndableListAdapter.notifyDataSetChanged();
+                            }
+
                         }
 
-                    }
+                        @Override
+                        public void onFailure(Call<Contents> call, Throwable t) {
 
-                    @Override
-                    public void onFailure(Call<Contents> call, Throwable t) {
-
-                    }
-                });
+                        }
+                    });
+        }
 
 
         expandableListView.setAdapter(exapndableListAdapter);
@@ -153,26 +159,28 @@ public class BookBaseActivity extends AppCompatActivity
                     Log.d(TAG, "onChildClick: " + thisChapter.getPath());
                     String[] chapters = thisChapter.getPath().split("\\/");
                     Log.d(TAG, "onChildClick: " + chapters[0] + " " + chapters[1]);
-                    API.getInstance().retrofit
-                            .create(GitbookAPI.class)
-                            .getThisBookContent(
-                                    bookAuthor,
-                                    bookId,
-                                    chapters[0],
-                                    chapters[1]
-                            )
-                            .enqueue(new Callback<BookData>() {
-                                @Override
-                                public void onResponse(Call<BookData> call, Response<BookData> response) {
-                                    Log.d(TAG, "onResponse: " + response.body());
-                                    ((BookPageFragment) fragment).replacePage((response.body().getSections().get(0).getContent()));
-                                }
+                    if (UtilityMethods.isNetworkConnected(BookBaseActivity.this)) {
+                        API.getInstance(BookBaseActivity.this).retrofit
+                                .create(GitbookAPI.class)
+                                .getThisBookContent(
+                                        bookAuthor,
+                                        bookId,
+                                        chapters[0],
+                                        chapters[1]
+                                )
+                                .enqueue(new Callback<BookData>() {
+                                    @Override
+                                    public void onResponse(Call<BookData> call, Response<BookData> response) {
+                                        Log.d(TAG, "onResponse: " + response.body());
+                                        ((BookPageFragment) fragment).replacePage((response.body().getSections().get(0).getContent()));
+                                    }
 
-                                @Override
-                                public void onFailure(Call<BookData> call, Throwable t) {
-                                    Log.d(TAG, "onFailure: " + t.getMessage());
-                                }
-                            });
+                                    @Override
+                                    public void onFailure(Call<BookData> call, Throwable t) {
+                                        Log.d(TAG, "onFailure: " + t.getMessage());
+                                    }
+                                });
+                    }
                 }
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
@@ -204,6 +212,5 @@ public class BookBaseActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
 
 }
